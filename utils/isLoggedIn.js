495af4817e54dc2817982d4dac453abdc02a91f6
Myron Lacey <mjlx42@gmail.com>
2020-12-30 04:27:18 -0500

    Add flash error message

--- utils/isLoggedIn.js
+++ utils/isLoggedIn.js
@@ -2,8 +2,9 @@ const isLoggedIn = (req, res, next) => {
 	if (req.isAuthenticated()) {
 		return next();
 	} else {
+		req.flash("error", "Sign in to acccess more features.");
 		res.redirect('/login');
 	}
-}
+};
 
 module.exports = isLoggedIn;
